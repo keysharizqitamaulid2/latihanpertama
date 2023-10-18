6aec0236c4efe2a562606318d512fe2c96275a48
Keysha <keysharizqitamaulid2@gmail.com>
2023-10-18 13:55:51 +0700

    Latihan Pertama - Keysha Rizqita Maulid XI RPl 1

---
+++ app/src/test/java/LoginActivity.kt
@@ -0,0 +1,10 @@
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_relative)
+    }
+}
